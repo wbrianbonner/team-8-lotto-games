ae2722cbbe98759b0d947cc27242a9b8606d6f86
Brian Bonner <wbrianbonner@gmail.com>
2022-11-17 21:13:49 +0000

    draft Position class

---
+++ LevelUpGame/levelup/cli/Position.cs
@@ -0,0 +1,14 @@
+using System.Drawing;
+namespace levelup.cli
+{
+class Position
+{    
+    Point coordinates;
+
+    public Position(int xCoordinates, int yCoordinates)
+    {
+      this.coordinates.X = xCoordinates;
+      this.coordinates.Y = yCoordinates;
+    } 
+}
+}
